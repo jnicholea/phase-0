ba23f8eaf6ce8b0b0fc8be23765d9b5a60f00702
Jenna Andersen <jnicholea@gmail.com>
2015-09-08 10:02:54 -0700

    add address define-method math and variables-methods

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,13 @@
+# Format an Address
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+def make_address(street, city, state, zip)
+end
+
+make_address("12078 Division S", "Royal City", "WA", 99357)
+
+p "You live at " + street + " in " + city + " " + state + " with the zipcode " + zip.to_s 
+
---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,31 @@
+# Math Methods
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+	num_1.to_i + num_2.to_i
+# add(10, 5)
+end
+
+def subtract(num_1, num_2)
+ num_1.to_i - num_2.to_i
+# subtract(10, 5)
+end
+
+def multiply(num_1, num_2)
+ num_1.to_i * num_2.to_i
+# subtract(10, 5)
+end
+
+def divide(num_1, num_2)
+ num_1.to_f / num_2.to_f
+# divide(10, 5)
+end
+
+p add(10, 5)
+p subtract(10, 5)
+p multiply(10, 5)
+p divide(10, 5)
