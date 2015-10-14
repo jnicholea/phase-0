3814427e2ad5fd80ac8f29444703ffdb7f118e2b
Jenna Andersen <jnicholea@gmail.com>
2015-10-13 17:31:32 -0700

    Update review.js

---
+++ week-9/review.js
@@ -0,0 +1,23 @@
+//Pseudocode
+// 1. Create a die-rolling game that prompts user to enter a guess, rolls the die, 
+// and checks whether the roll matches the guess.
+// 2. Initialize a die with six-sides.
+// 3. Prompt for guess. Store user guess.
+// 4. Computer selects random number between 1-6. 
+// 5. Check to see if user guess and random number match.
+// 6. Declare winner if it is a match, encourage to play again if not. 
+
+//Initial solution
+
+//solution here: https://github.com/jnicholea/phase-0/blob/master/week-9/jsdiegame.html
+ 
+
+//Reflection
+// - What concepts did you solidify in working on this challenge?
+//     - I styled the page using CSS, formatted the page using HTML, wrote the functions using Javascript, and integrated JQuery as well. I’m so proud of my work today!
+// - What was the most difficult part of this challenge?
+//     - Everything that involved figuring out how to make actions happen on click. Swapping out the images for a win/loss took me awhile because I was having trouble with scope.
+// - Did you solve the problem in a new way this time?
+//     - My genius BIL is staying with us and he is fabulous tech support. I wish I had that all the time.
+// - Was your pseudocode different from the Ruby version?
+//     - I didn’t pay much attention. I knew I wanted to use the “die challenge” loosely and make a game around that concept.
